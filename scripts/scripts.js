ea9c5139df96619c7b5e03165dff80b3eae6fb7c
FacundoBeldi <facu.beldi@hotmail.com>
2024-06-22 02:03:12 -0300

    agregar user

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,6 +17,7 @@ function login(event) { //funcion del login, paso como parametro event para la f
 
 const baseURL = 'https://66677fa2a2f8516ff7a7a5f0.mockapi.io' //URL de la API
 
+//? Obtener los usuarios de la API
 axios.get(`${baseURL}/users`)//Obtengo los usuarios de la API
 .then(response => {
     renderUsers(response.data)
@@ -31,7 +32,7 @@ axios.get(`${baseURL}/users`)//Obtengo los usuarios de la API
 console.log(error);
 }); //Si hay un error, lo muestro con esta alerta de swal.fire
 
-//Rellenar la tabla de usuarios de la API, y los agregados por el usuario
+//? Rellenar la tabla de usuarios de la API, y los agregados por el usuario
 
 const tableHTML = document.getElementById('table-container');
 const tableBodyHTML = document.getElementById('table-body');
@@ -47,7 +48,7 @@ function renderUsers(arrayUsers) {
                 <td class="user-name"> ${user.fullname}</td>
                 <td class="user-mail"> ${user.email}</td>
                 <td class="user-city"> ${user.location}</td>
-                <td class="user-age"> ${user.age}</td>
+                <td class="user-age"> ${user.bornDate}</td>
                 <td class="cont-btn">
                     <button class="btn btn-danger btn-sm" onclick="deleteUser('${user.id}')">
                         <i class="fa-solid fa-trash"></i>
@@ -58,4 +59,24 @@ function renderUsers(arrayUsers) {
                 </td>
             </tr>`;
     });
+    //updateEditButtons();
+}
+
+//? Editar un usuario
+
+function updateEditButtons(){
+    userButtonsEdit = document.querySelectorAll('button[data-edit]'); //Selecciono todos los botones con el atributo data-edit
+    userButtonsEdit.forEach(button => { //Por cada botón, le agrego un evento click
+        button.addEventListener('click', function(event){
+            event.preventDefault(); //Que no se recargue la página
+            const id = event.currentTarget.dataset.edit; //Obtengo el id del usuario que fue clickeado
+            completeUser(id); //Voy a la función de completar usuario con el id clickeado
+        });
+    });
+}
+
+//? Agregar un usuario
+
+function completeUser(){
+    
 }
